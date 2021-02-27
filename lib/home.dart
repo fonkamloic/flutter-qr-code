5de2a898c0bb16d5ac5a3b48d0d689a73c85412c
Fonkam Loïc <fonkamloic@gmail.com>
2021-02-27 04:32:06 +0100

    Started code

---
+++ lib/home.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flushbar/flushbar_helper.dart';
+import 'package:flutter/services.dart';
+import 'package:qr_refresh/qr_code_page.dart';
+
+class QRHome extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final scanedSeed = useState('Flutter Developers Cameroon 💙');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('QR challenge Home'),
+      ),
+      body: Center(
+        child: Text(scanedSeed.value,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+      ),
+      floatingActionButton: SpeedDial(
+          marginRight: 18,
+          marginBottom: 20,
+          animatedIcon: AnimatedIcons.add_event,
+          animatedIconTheme: const IconThemeData(size: 22.0),
+          closeManually: false,
+          curve: Curves.bounceIn,
+          overlayColor: Colors.black,
+          overlayOpacity: 0.5,
+          onOpen: () => print('OPENING DIAL'),
+          onClose: () => print('DIAL CLOSED'),
+          tooltip: 'Speed Dial',
+          heroTag: 'speed-dial-hero-tag',
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          elevation: 8.0,
+          shape: const CircleBorder(),
+          children: [
+            SpeedDialChild(
+              child: const Icon(Icons.code),
+              backgroundColor: Colors.red,
+              label: 'QR code',
+              labelStyle: const TextStyle(fontSize: 18.0),
+              onTap: () => Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => QRCodeScreen())),
+            ),
+            SpeedDialChild(
+              child: const Icon(Icons.camera),
+              backgroundColor: Colors.blue,
+              label: 'Scan',
+              labelStyle: const TextStyle(fontSize: 18.0),
+              onTap: () => scan(context, scanedSeed),
+            ),
+          ]),
+    );
+  }
+}
+
+Future scan(BuildContext context, ValueNotifier<String> seed) async {
+  try {
+    final ScanResult barcode =
+        await BarcodeScanner.scan(options: const ScanOptions());
+
+    seed.value = barcode.rawContent;
+  } on PlatformException catch (e) {
+    if (e.code == BarcodeScanner.cameraAccessDenied) {
+      FlushbarHelper.createError(
+              message: 'The user did not grant the camera permission!')
+          .show(context);
+      // barcodeState.value = 'The user did not grant the camera permission!';
+    } else {
+      FlushbarHelper.createError(message: 'Unknown error: $e').show(context);
+    }
+  } on FormatException {} catch (e) {
+    // FlushbarHelper.createError(message: 'Unknown error: $e'.i18n).show(context);
+  }
+}
---
+++ lib/qr_code_page.dart
@@ -0,0 +1,126 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_countdown_timer/index.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class QRCodeScreen extends HookWidget {
+  CountdownController countdownController;
+  Map<String, Object> seed = Map();
+  SharedPreferences sharedPreferences;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    final refresh = useState(false);
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('QrCode'),
+        ),
+        body: Container(
+          child: Center(
+            child: FutureBuilder<Map<String, Object>>(
+                future: setSeed(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (snapshot.connectionState == ConnectionState.done &&
+                      snapshot.hasData) {
+                    Timer.periodic(Duration(seconds: 13), (timer) {
+                      refresh.value = !refresh.value;
+                    });
+                    seed = snapshot.data;
+                    final String cache = seed['seed'];
+                    sharedPreferences.setString("cache", cache);
+
+                    print("Data is ${snapshot.data['seed']}");
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        QrImage(
+                          data: seed['seed'],
+                          version: QrVersions.auto,
+                          // embeddedImage: AssetImage(),
+                          size: size.width * 0.57,
+                          embeddedImageStyle: QrEmbeddedImageStyle(
+                            size: Size(size.width * 0.08, size.width * 0.08),
+                          ),
+                        ),
+                        Countdown(
+                            countdownController: countdownController,
+                            builder: (context, time) {
+                              return Text("${time.inSeconds}s");
+                            }),
+                        // Text('${timer.value}
+                      ],
+                    );
+                  } else if (snapshot.connectionState == ConnectionState.done &&
+                      snapshot.hasError) {
+                    Timer.periodic(Duration(seconds: 13), (timer) {
+                      refresh.value = !refresh.value;
+                    });
+                    seed = snapshot.data;
+                    final String cache = sharedPreferences
+                        .getString("cache" ?? "No data was cached");
+
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        QrImage(
+                          data: cache,
+                          version: QrVersions.auto,
+                          // embeddedImage: AssetImage(),
+                          size: size.width * 0.57,
+                          embeddedImageStyle: QrEmbeddedImageStyle(
+                            size: Size(size.width * 0.08, size.width * 0.08),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'Not Internet: Cached Data',
+                          style: TextStyle(color: Colors.redAccent),
+                        ),
+                        // Text('${timer.value}
+                      ],
+                    );
+                  }
+                  return Container();
+                }),
+          ),
+        ));
+  }
+
+  Future<Map<String, Object>> getSeed() async {
+    const String url =
+        'https://fq741r6kaj.execute-api.us-east-2.amazonaws.com/generate/seed';
+    try {
+      final http.Response response = await http.get(url);
+      if (response.statusCode == 200) {
+        Map<String, Object> decodeMap = json.decode(response.body);
+        print(decodeMap);
+        return Future.value(decodeMap);
+      }
+      return null;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  Future<Map<String, Object>> setSeed() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    seed = await getSeed();
+    countdownController = CountdownController(
+        duration: Duration(
+            seconds:
+                DateTime.fromMillisecondsSinceEpoch(seed["expires_at"] as int)
+                    .difference(DateTime.now())
+                    .inSeconds))
+      ..start();
+
+    return seed;
+  }
+}
