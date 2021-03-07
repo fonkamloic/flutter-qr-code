404b6ba0b5b5069f067afa66832da1c5310bdd7d
Fonkam Loïc <fonkamloic@gmail.com>
2021-03-07 18:54:02 +0100

    done

--- lib/qr_code_page.dart
+++ lib/qr_code_page.dart
@@ -1,17 +1,10 @@
 import 'dart:async';
-import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_countdown_timer/index.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:qr_flutter/qr_flutter.dart';
-import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_countdown_timer/index.dart';
-import 'package:qr_flutter/qr_flutter.dart';
 
 import 'application/seed_gen_bloc/seed_gen_bloc.dart';
 import 'core/my_cache.dart';
@@ -48,7 +41,7 @@ class QRCodeBody extends StatelessWidget {
                     const Center(child: CircularProgressIndicator()),
                 fetchFailed: (e) {
                   //let try to fetch after 15sec
-                  Future.delayed(const Duration(seconds: 15), () {
+                  Future.delayed(const Duration(seconds: 5), () {
                     context
                         .bloc<SeedGenBloc>()
                         .add(const SeedGenEvent.getSeed());
